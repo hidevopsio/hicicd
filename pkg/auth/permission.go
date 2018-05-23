d78fbf62fb1bcf025fb2273bab22c918f54783b8
Chu Lei <chu.leilei@vpclub.cn>
2018-05-23 10:04:41 +0800

    please delete this:  pkg/ci/impl/config and commit again

--- pkg/auth/permission.go
+++ pkg/auth/permission.go
@@ -6,29 +6,29 @@ import (
 )
 
 type Permission struct {
-	Product scm.ProjectInterface
-	ProductMember scm.ProjectMemberInterface
+	Project scm.ProjectInterface
+	ProjectMember scm.ProjectMemberInterface
 }
 
 type PermissionInterface interface {
 	Get(baseUrl, token, name, namespace string, uid int) (string, string, int, error)
 }
 
-func (a *Permission) Get(baseUrl, token, name, namespace string, uid int) (string, string, int, error) {
+func (p *Permission) Get(baseUrl, token, name, namespace string, uid int) (string, string, int, error) {
 	scmFactory := new(factories.ScmFactory)
 	var err error
-	a.Product, err = scmFactory.NewProject(factories.GitlabScmType)
+	p.Project, err = scmFactory.NewProject(factories.GitlabScmType)
 	if err != nil {
 		return "", "", 0, err
 	}
-	pid, err := a.Product.GetUserProject(baseUrl, token, name, namespace)
+	pid, err := p.Project.GetUserProject(baseUrl, token, name, namespace)
 	if err != nil {
 		return "", "", 0, err
 	}
-	a.ProductMember, err = scmFactory.NewProjectMember(factories.GitlabScmType)
+	p.ProjectMember, err = scmFactory.NewProjectMember(factories.GitlabScmType)
 	if err != nil {
 		return "", "", 0, err
 	}
-	metaName, roleRefName, accessLevelValue, err := a.ProductMember.GetProjectMember(token, baseUrl, pid, uid)
+	metaName, roleRefName, accessLevelValue, err := p.ProjectMember.GetProjectMember(token, baseUrl, pid, uid)
 	return metaName, roleRefName, accessLevelValue, err
 }
